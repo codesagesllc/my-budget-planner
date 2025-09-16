7ba2fd80bcb5978cfae44ae6c1a17dda77113f2f
Carleton Cabarrus <carleton.cabarrus@gmail.com>
2025-09-15 22:24:04 -0400

    commit vercel deployment

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, InputHTMLAttributes, ReactNode } from 'react'
+import { InputHTMLAttributes, ReactNode } from 'react'
 import { cn } from '@/lib/utils'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
@@ -7,43 +7,40 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   icon?: ReactNode
 }
 
-const InputComponent = forwardRef<HTMLInputElement, InputProps>(
-  ({ className, label, error, icon, ...props }, ref) => {
-    return (
-      <div>
-        {label && (
-          <label className="block text-sm font-medium text-gray-700 mb-2">
-            {label}
-          </label>
+export function Input({
+  className,
+  label,
+  error,
+  icon,
+  ...props
+}: InputProps) {
+  return (
+    <div>
+      {label && (
+        <label className="block text-sm font-medium text-gray-700 mb-2">
+          {label}
+        </label>
+      )}
+      <div className="relative">
+        {icon && (
+          <div className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400">
+            {icon}
+          </div>
         )}
-        <div className="relative">
-          {icon && (
-            <div className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400">
-              {icon}
-            </div>
+        <input
+          className={cn(
+            'w-full rounded-md border border-gray-300',
+            'focus:ring-2 focus:ring-blue-500 focus:border-transparent',
+            icon ? 'pl-10 pr-4 py-2' : 'px-4 py-2',
+            error && 'border-red-500',
+            className
           )}
-          <input
-            ref={ref}
-            className={cn(
-              'w-full rounded-md border border-gray-300',
-              'focus:ring-2 focus:ring-blue-500 focus:border-transparent',
-              icon ? 'pl-10 pr-4 py-2' : 'px-4 py-2',
-              error && 'border-red-500',
-              className
-            )}
-            {...props}
-          />
-        </div>
-        {error && (
-          <p className="mt-1 text-sm text-red-600">{error}</p>
-        )}
+          {...props}
+        />
       </div>
-    )
-  }
-)
-
-InputComponent.displayName = 'Input'
-
-export function Input(props: InputProps) {
-  return <InputComponent {...props} />
+      {error && (
+        <p className="mt-1 text-sm text-red-600">{error}</p>
+      )}
+    </div>
+  )
 }
