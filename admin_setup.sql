213e27f1dd248c9210cc75088c997f8e385e83b0
Carleton Cabarrus <carleton.cabarrus@gmail.com>
2025-09-20 19:05:51 -0400

    removed unncessary files

--- admin_setup.sql
+++
@@ -1,9 +0,0 @@
--- Set admin user
-UPDATE public.users SET is_admin = true WHERE email = 'email address here';
-
--- Set free trial dates for existing users
-UPDATE public.users
-SET 
-  free_trial_start_date = COALESCE(free_trial_start_date, created_at),
-  free_trial_end_date = COALESCE(free_trial_end_date, created_at + INTERVAL '14 days')
-WHERE subscription_tier = 'free_trial';
